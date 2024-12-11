5a614a151f90d373ca85732d09aa3575ce05545d
SAMW3L <samuelsarah678@gmail.com>
2024-12-11 17:54:44 +0300

    ..

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Download } from 'lucide-react';
-
+import { useNavigate } from 'react-router-dom';
 import CVPDF from '../../Assets/SAMWELCV.pdf';
+import ProfilePIC from '../../Assets/images/profile.jpg';
+
+
 
 const Hero = () => {
+
+  const handlePDF = () => {
+    // Open the PDF in a new tab
+    window.open(CVPDF, '_blank');
+  };
+
+  
   return (
     <section id="home" className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-50 pt-16">
       <div className="container mx-auto px-4">
@@ -27,7 +37,8 @@ const Hero = () => {
             </p>
             <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
               <a 
-                href={CVPDF} 
+                href='#'
+                onClick={handlePDF} 
                 download
                 className="bg-blue-600 text-white px-6 py-3 rounded-lg flex items-center gap-2 hover:bg-blue-700 transition-colors"
               >
@@ -50,7 +61,7 @@ const Hero = () => {
             transition={{ duration: 0.5, delay: 0.2 }}
           >
             <img 
-              src="Assets/images/profile.jpg" 
+              src={ProfilePIC}
               alt="profile"
               className="rounded-full w-64 h-64 md:w-96 md:h-96 object-cover mx-auto shadow-xl"
             />
